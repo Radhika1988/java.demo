c720eff1126bcded3fb8e5c8f8cbedc48a24850e
radhika <radhikasurineni@gmail.com>
2024-06-28 20:42:51 +0530

    commit from Phone

---
+++ src/Contact/Cotact.java
@@ -0,0 +1,15 @@
+package Contact;
+
+public class Cotact {
+    public String name;
+    public int PhoneNumber;
+    public String type;
+ public static void main(String[] args){
+     Cotact cotact = new Cotact();
+     cotact.name="SAINEHA";
+     cotact.PhoneNumber = 9000955590;
+
+
+}
+
+}
